44681ea51ebb2d5be6fc03ea3c884607038ef8ed
salokhiddinusmonovich <usmonovsalokhiddin@example.com>
2024-12-15 00:34:29 +0500

    regitration

--- config/urls.py
+++ config/urls.py
@@ -49,7 +49,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-# Add static and media files handling (only for development)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
