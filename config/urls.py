ba1ebf937c9c470df9dcf0a325d2fec1c1227f58
salokhiddinusmonovich <usmonovsalokhiddin@example.com>
2024-12-14 19:28:39 +0500

    regitration

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,4 +38,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
