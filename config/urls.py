be82829de62eba402fea5c4d9587eed7f9fc8f13
salokhiddinusmonovich <usmonovsalokhiddin@example.com>
2024-12-15 00:24:19 +0500

    regitration

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,8 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -46,3 +48,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# Add static and media files handling (only for development)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
